388098965ec025d5546c55b03b7c90ad8c80d624
Arbeiter2 <petterbight@gmail.com>
2016-11-15 18:04:40 +0100

    New

---
+++ src/RecreationBuilding.java
@@ -0,0 +1,8 @@
+
+abstract public class RecreationBuilding extends MunicipalBuilding {
+	
+	public RecreationBuilding(int constrMonth, int cover, int basicCost, int mthlyCost) {
+		super(constrMonth, cover, basicCost, mthlyCost);
+	}
+
+}
