0ef7b3aa058ef747fee951146fbc203e7d852677
Arbeiter2 <petterbight@gmail.com>
2016-11-24 00:22:18 +0100

    Costs

--- src/CitySimulationGame.java
+++ src/CitySimulationGame.java
@@ -3,6 +3,10 @@ public class CitySimulationGame {
 
 	CitySimulation citySim;
 	
+	private static final int DEFAULT_HEIGHT = 20;
+	private static final int DEFAULT_WIDTH = 20;
+	private static final double DEFAULT_INITIAL_BUDGET = 1000000;
+	
 	public CitySimulationGame(int width, int height, int startBudget) {
 		citySim = new CitySimulation(width, height, startBudget);
 	}
